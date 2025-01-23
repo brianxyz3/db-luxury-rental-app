f4bfe0dd48c6560bf18fb768285d8e588693d37e
BrianXYZ <brianchima22@gmail.com>
2025-01-23 13:20:33 +0100

    added call to action with link to contact using email

---
+++ src/components/GetStarted.jsx
@@ -0,0 +1,13 @@
+
+const GetStarted = () => {
+    return (
+        <div className="mx-4 mt-10 font-mono h-40 flex justify-center items-center">
+            <div className="bg-gray-400 text-center flex flex-col gap-1 items-center py-3 border-2 border-dashed border-black rounded-3xl md:gap-2">
+                <h3 className="text-blue-900 font-bold text-3xl w-2/3 md:w-1/2 md:text-4xl xl:text-5xl"><span className="text-nowrap">Let's Work</span> Together!</h3>
+                <a href="mailto:dbnigltd.com@yahoo.com" className="bg-blue-900 text-white text-sm md:text-base xl:text-lg p-2 mt-1 rounded-xl hover:font-bold hover:rounded-lg hover:-translate-y-1 hover:shadow-lg duration-100">CONTACT US</a>
+            </div>
+        </div>
+    )
+}
+
+export default GetStarted;
