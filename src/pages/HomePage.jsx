c5469e683ea5abb73b30a504f1453ad822dea3b7
BrianXYZ <brianchima22@gmail.com>
2025-02-01 18:31:45 +0100

    added alt text to images on webpage and refactored service page to improve readability and ease of updating code

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,14 +5,14 @@ import AboutUs from "../components/AboutUs";
 import GetStarted from "../components/GetStarted";
 import Header from "../components/Header";
 
-const Home = () => {
+const HomePage = () => {
     return (
         <>
             <Header bgImg="bg-homePageHeaderImg bg-right h-dvh">
                 <div className="w-4/5 md:w-3/4 text-xl text-[rgb(14,14,215)] text_shadow md:text-3xl lg:text-5xl font-mono font-bold mx-auto">
                     <h1 className="text-nowrap">Experience Luxury</h1>
                     <h2>On The Road</h2>
-                    <p className="md:mt-4 mt-4 text-sm md:text-lg lg:text-xl xl:text-2xl font-thin">Discover the perfect blend of elegance and <span className="md:text-nowrap">perfomance in every journey.</span></p>
+                    <p className="md:mt-4 mt-4 text-sm md:text-lg lg:text-xl xl:text-2xl font-thin">Discover the perfect blend of elegance and <span className="md:text-nowrap">perfomance in every drive.</span></p>
                 </div>
                 <button className="bg-[rgb(14,14,215)] text-sm md:text-lg px-3 py-1 rounded-xl mt-2 text-white font-bold hover:animate-bounce hover:shadow-lg focus:animate-bounce shadow-black">Get Started <ArrowCircleRightOutlined /></button>
             </Header>
@@ -22,6 +22,7 @@ const Home = () => {
                     <div className="w-full flex justify-start">
                         <FeaturesCard
                             img="../src/assets/images/featuresImg1.jpg"
+                            imgAltText="a photo of a man's hand on the steering wheel of a mercedes benz"
                             description="Choose from our exclusive fleet of world-class luxury vehicles. From sleek sports cars to sophisticated sedans and SUVs, each car is meticulously maintained to provide an unparalleled driving experience for any occasion."
                         >
                             <h3 className="text-2xl px-2 font-bold">Drive the Extraordinary</h3>
@@ -30,6 +31,7 @@ const Home = () => {
                     <div className="w-full flex justify-end">
                         <FeaturesCard
                             img="../src/assets/images/chauffeurImg.jpg"
+                            imgAltText="a photo of an african male dressed in a suit using his mobile phone in the backseat of a luxury vehicle"
                             description="Relax and enjoy the journey with our professional chauffeur services. Whether for business meetings, special events, or airport transfers, our experienced drivers ensure a seamless, first-class experience."
                         >
                             <h3 className="text-2xl px-2 font-bold">Luxury in motion, <span className="text-nowrap">Hands-Free</span></h3>
@@ -38,6 +40,7 @@ const Home = () => {
                     <div className="w-full flex justify-start">
                         <FeaturesCard
                             img="../src/assets/images/weddingCarImg2.jpg"
+                            imgAltText="a photo of a white rolls royce parked in front of a building"
                             description="Make your special day even more magical with a luxurious wedding car. From timeless classics to modern marvels, we've got the right car tailor-made for the occassion. We’ll help you arrive in style."
                         >
                             <h3 className="text-2xl px-2 font-bold">Arrive in Elegance</h3>
@@ -46,6 +49,7 @@ const Home = () => {
                     <div className="w-full flex justify-end">
                         <FeaturesCard
                             img="../src/assets/images/altHeroImg.jpg"
+                            imgAltText="a photo of a sports car driving down a long, open, winding country road"
                             description="Enjoy the comfort and prestige of a luxury vehicle on your terms. Our long-term rental options combine flexibility and exclusivity for those who value extended elegance."
                         >
                             <h3 className="text-2xl px-2 font-bold">Luxury for the Long Haul</h3>
@@ -59,4 +63,4 @@ const Home = () => {
     )
 }
 
-export default Home;
+export default HomePage;
--- src/pages/ServicesPage.jsx
+++ src/pages/ServicesPage.jsx
@@ -4,7 +4,7 @@ import Header from "../components/Header";
 import "../stylesheets/carousel.css"
 
 const ServicesPage = () => {
-    const firstCardColumn = [
+    const firstServiceColumn = [
         {
             title: "Premium Luxury Car Rentals",
             description: "Indulge in first-class travel with our high-end vehicles, designed to elevate every trip. Whether it’s a weekend getaway or a long road trip, enjoy unmatched comfort and luxury.",
@@ -19,11 +19,11 @@ const ServicesPage = () => {
         },
         {
             title: "Tailored Luxury, Just for You",
-            description: "Create your dream driving experience with our personalized services. Whether you need a specific vehicle or unique arrangements, our team will make it happen.",
+            description: "Create your dream driving experience with our personalized services. Whether you need a specific vehicle like a bulletproof suv or unique arrangements, our team will make it happen.",
         },
     ];
 
-    const secondCardColumn = [
+    const secondServiceColumn = [
         {
             title: "Airport Transfers",
             description: "Start or end your journey in style with our premium airport transfer services. Enjoy punctuality, comfort, and exceptional service tailored to your needs.",
@@ -44,24 +44,68 @@ const ServicesPage = () => {
 
     const images = [
         {
-            src: "../src/assets/images/heroImg.jpg",
-            altText: "",
-            id: "image1"
+            src: "../src/assets/images/toyotaImg1.jpg",
+            altText: "a photo of a black bulletproof suv, toyota land cruiser jeep 2022 model owned by DB car hire",
         },
         {
-            src: "../src/assets/images/altHeroImg.jpg",
-            altText: "",
-            id: "image2"
+            src: "../src/assets/images/lexusImg2.jpg",
+            altText: "a photo of a black suv, lexus gx 460 2022 model owned by DB car hire",
         },
         {
-            src: "../src/assets/images/chauffeurImg.jpg",
-            altText: "",
-            id: "image3"
+            src: "../src/assets/images/landcruiserImg2.jpg",
+            altText: "a photo of a black suv, toyota prado jeep 2021 model owned by DB car hire",
         },
         {
-            src: "../src/assets/images/featuresImg1.jpg",
-            altText: "",
-            id: "image4"
+            src: "../src/assets/images/landcruiserImg3.jpg",
+            altText: "a photo of a black suv, toyota prado jeep 2021 model owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/rollsRoyceImg1.jpg",
+            altText: "a photo of a black rolls royce owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/pradoImg1.jpg",
+            altText: "a photo of a black suv, toyota prado jeep 2020 model owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/landcruiserImg1.jpg",
+            altText: "a photo of a black suv, toyota land cruiser 2020 model owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/toyotaBusImg1.jpg",
+            altText: "a photo of a white toyota coaster bus owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/lexusImg1.jpg",
+            altText: "a photo of a white suv, lexus gx 460 2022 model owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/mercedesImg1.jpg",
+            altText: "a photo of a dark blue suv, mercedes benz gle 350 owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/hiluxImg1.jpg",
+            altText: "a photo of a white suv, toyota hilux 2022 model owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/lexusImg3.jpg",
+            altText: "a photo of a black bulletproof suv, lexus lx 600 2022 model owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/lexusImg4.jpg",
+            altText: "a photo of a black suv, lexus sport 2020 model owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/mercedesImg2.jpg",
+            altText: "a photo of a black suv, mercedes benz gle 350 2020 model owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/rangeRoverImg1.jpg",
+            altText: "a photo of a black suv, range rover autobiography owned by DB car hire",
+        },
+        {
+            src: "../src/assets/images/carFleetImg1.jpg",
+            altText: "a photo of several luxury black suv from DB car hire's fleet",
         },
     ];
 
@@ -72,14 +116,14 @@ const ServicesPage = () => {
                 <div className="w-4/5 md:w-3/4 text-xl text-[rgb(1,50,250)] text_shadow md:text-3xl lg:text-5xl font-bold mx-auto">
                     <h2 className="text-lg md:text-3xl lg:text-5xl">Your Journey to Exceptional Begins Here</h2>
                     {/* Drive Your Dream, Anywhere, Anytime. */}
-                    <p className="text-xs md:text-base lg:text-lg xl:text-xl mt-6">Experience the ease of booking premium vehicles. Browse, book, and hit the road in style. Whether it’s a business trip, a special event, or a weekend getaway, we make luxury travel effortless and affordable.</p>
+                    <p className="text-xs md:text-base lg:text-lg xl:text-xl mt-5 landscape:mt-2 md:landscape:mt-8">Experience the ease of booking premium vehicles. Browse, book, and hit the road in style. Whether it’s a business trip, a special event, or a weekend getaway, we make luxury travel effortless and affordable.</p>
                 </div>
             </Header>
             <main className="mt-20">
                 <section className="flex justify-evenly items-center flex-wrap flex-grow md:items-start md:flex-nowrap gap-4 md:gap-0">
-                    <div className="md:w-[27%] w-4/5 flex flex-col gap-5">
-                        {firstCardColumn.map((card, idx) => (
-                            <Card key={idx} style="bg-white shadow-lg h-72 lg:h-56 duration-200 hover:-translate-y-2" title={card.title} description={card.description} />
+                    <div className="md:w-[27%] w-3/4 flex flex-col gap-3">
+                        {firstServiceColumn.map((card, idx) => (
+                            <Card key={idx} style="bg-white shadow-lg h-56 md:h-72 lg:h-56 duration-200 hover:-translate-y-2" title={card.title} description={card.description} />
                         ))
                         }
                     </div>
@@ -87,19 +131,19 @@ const ServicesPage = () => {
                     <Carousel>
                         {
                             images.map((image, idx) => (
-                                <img key={idx} id={image.id} src={image.src} alt={image.altText} />
+                                <img key={idx} id={`image${idx}`} src={image.src} alt={image.altText} />
                             ))
                         }
                         <div className="carousel_nav">
                             {images.map((image, idx) => (
-                                <a href={`#${image.id}`} key={idx}></a>
+                                <a href={`#image${idx}`} key={idx} className="hidden lg:block"></a>
                             ))}
                         </div>
                     </Carousel>
 
-                    <div className="md:w-[27%] w-4/5 flex flex-col gap-5">
-                        {secondCardColumn.map((card, idx) => (
-                            <Card key={idx + 4} style="bg-white shadow-lg h-72 lg:h-56 duration-200 hover:-translate-y-2" title={card.title} description={card.description} />
+                    <div className="md:w-[27%] w-3/4 flex flex-col gap-3">
+                        {secondServiceColumn.map((card, idx) => (
+                            <Card key={idx + 4} style="bg-white shadow-lg h-56 md:h-72 lg:h-56 duration-200 hover:-translate-y-2" title={card.title} description={card.description} />
                         ))
                         }
                     </div>
