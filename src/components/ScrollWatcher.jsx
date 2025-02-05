99472d043aa673338d2787f041e820e35ff69388
BrianXYZ <brianchima22@gmail.com>
2025-02-05 11:22:12 +0100

    added scroll watcher and to top button

---
+++ src/components/ScrollWatcher.jsx
@@ -0,0 +1,16 @@
+import { KeyboardDoubleArrowUp } from "@mui/icons-material";
+import "../stylesheets/ScrollWatcher.css";
+
+const ScrollWatcher = () => {
+    return (
+        <a href="#top" className="scroll_watcher_btn fixed bottom-10 right-5">
+            <div className="scroll_watcher">
+                <div className="bg-black flex justify-center items-center">
+                    <KeyboardDoubleArrowUp />
+                </div>
+            </div>
+        </a>
+    )
+}
+
+export default ScrollWatcher;
