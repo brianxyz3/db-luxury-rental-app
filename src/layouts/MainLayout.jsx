3ba1883eaface17d3c7802d771917a743584e799
BrianXYZ <brianchima22@gmail.com>
2025-02-26 12:21:40 +0100

    fixed component path bug in deployment to production

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router"
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import ScrollWatcher from "../components/ScrollWatcher";
 
